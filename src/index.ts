484f5194fe8db9b583f1664a893b446f0b4d626c
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-29 23:40:49 +0600

    learn how to use readOnly

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,11 @@
-interface User {
-    id:string,
-    name:string,
-    age:number,
-    email:string,
-    password:string
+type User = {
+   name:string;
+   age:number;
 }
 
-//select subset of the interface
-type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
 
-//makes all existing properties optional
-type updatePropsOptional = Partial<UpdateProps>
+const user:Readonly<User> = {
+    name:'mehedi',
+    age:34
+}
 
- function UpdateUser (updatedProps: updatePropsOptional){
-    
- }
-
- UpdateUser({
-    name:"mehedi",
-    age:34,
-   
-
- })
-
-
- function updateOptional (updateOptional:updatePropsOptional){
-    name:"hi@gamil.com"
- }    

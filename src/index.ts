0d69d95c0ddbfdced1f0dd0beb5ca06caa799ccb
Mehedi- Hasan <mhasanpolash123@gmail.com>
2026-04-30 00:59:53 +0600

    learn how to use Record for cleaner object declaration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-type User = {
-   name:string;
-   age:number;
+//Record user for cleaner object declaration
+
+type Users = Record<string, {name:string, age:number}>;
+
+const users:Users = {
+   "raju" : {name:"naim",age:23},
+   "naim" : {name:"nahid", age:55}
 }
-
-
-const user:Readonly<User> = {
-    name:'mehedi',
-    age:34
-}
-
